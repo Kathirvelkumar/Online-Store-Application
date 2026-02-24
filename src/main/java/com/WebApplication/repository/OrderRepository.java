14ab8411a9e19e42fc8ed88413aeeb9b70b0e7e5
Kathirvel K <kathirkumar1862004@gmail.com>
2026-02-24 19:01:43 +0530

    Modify Controllers using ResponseEntity

--- src/main/java/com/WebApplication/repository/OrderRepository.java
+++ src/main/java/com/WebApplication/repository/OrderRepository.java
@@ -2,10 +2,7 @@ package com.WebApplication.repository;
 
 import com.WebApplication.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import java.util.*;
-import org.springframework.data.domain.Pageable;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Orders, Long> {
