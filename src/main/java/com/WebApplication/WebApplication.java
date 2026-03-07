09c9dc28281abc58330b165d8a63687045a02fab
Kathirvel K <kathirkumar1862004@gmail.com>
2026-03-07 13:05:00 +0530

    Do some Trials

--- src/main/java/com/WebApplication/WebApplication.java
+++ src/main/java/com/WebApplication/WebApplication.java
@@ -1,11 +1,7 @@
 package com.WebApplication;
 
-import com.WebApplication.entity.Seats;
-import com.WebApplication.repository.SeatRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebApplication {
@@ -14,14 +10,14 @@ public class WebApplication {
         SpringApplication.run(WebApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner initSeats(SeatRepository seatRepository) {
-        return args -> {
-            for (int i = 1; i <= 60; i++) {
-                if (!seatRepository.existsById(i)) {
-                    seatRepository.save(new Seats(i, false));
-                }
-            }
-        };
-    }
+//    @Bean
+//    CommandLineRunner initSeats(SeatRepository seatRepository) {
+//        return args -> {
+//            for (int i = 1; i <= 60; i++) {
+//                if (!seatRepository.existsById(i)) {
+//                    seatRepository.save(new Seats(i, false));
+//                }
+//            }
+//        };
+//    }
 }
--- src/main/java/com/WebApplication/repository/SeatRepository.java
+++
@@ -1,8 +0,0 @@
-package com.WebApplication.repository;
-import com.WebApplication.entity.Seats;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface SeatRepository extends JpaRepository<Seats, Integer> {
-
-}
